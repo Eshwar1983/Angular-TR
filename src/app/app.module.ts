c6a051eb9680c9230925994a2c72583f62359b00
Eshwar1983 <Eshwargowda19@gmail.com>
2017-08-16 18:40:32 +0530

    14th-branch first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './common/header/header.component';
 import { FooterComponent } from './common/footer/footer.component';
 import { WrapperComponent } from './common/wrapper/wrapper.component';
+import { SpinnerComponent } from './common/spinner/spinner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    WrapperComponent
+    WrapperComponent,
+    SpinnerComponent
   ],
   imports: [
     BrowserModule
